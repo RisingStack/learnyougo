cab48488241c9362135edbdb6a5f34e13d9e6a8d
Peter Czibik <p.czibik@gmail.com>
2018-02-11 14:53:01 +0100

    feat(exercises): add template for creating exercises

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This is a go port of the original [learnyounode](https://github.com/workshopper/learnyounode) workshop, that helped thousands of developers all around the world to learn nodejs on their own or during hosted workshop sessions.
 
 Our initiative is to bring the same workshopper experience to the go community. Our goal is to make learning go more approachable for everyone.
+
+## Building
+
+We're bundling assets into the binary file using the [go-bindata package](https://github.com/a-urth/go-bindata) package. To run the project first generate the `bindata.go` file using `go-bindata ./exercises`
--- workshopper.go
+++ workshopper.go
@@ -2,41 +2,65 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"strconv"
 
-	"github.com/jroimartin/gocui"
+	"gopkg.in/russross/blackfriday.v2"
 )
 
+type example struct {
+	Name string
+}
+
 func main() {
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer g.Close()
-
-	g.SetManagerFunc(layout)
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
-}
-
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	v := "Choose your destiny!"
-	if v, err := g.SetView("hello", maxX/2-len(v), maxY/2, maxX/2+len(v), maxY/2+2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		index, err := Asset("data/index.tmpl")
+		if err != nil {
+			panic(err)
 		}
-		fmt.Fprintln(v, "Choose your destiny!")
-	}
-	return nil
-}
+		fmt.Fprint(w, string(index))
+	})
 
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
+	http.HandleFunc("/exercises", func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			panic(err)
+		}
+		number, err := strconv.Atoi(r.PostForm.Get("number"))
+		if err != nil {
+			panic(err)
+		}
+		exerciseTemplateName := fmt.Sprintf("data/exercises/%d/exercise.go.tmpl", number)
+		exerciseTestTemplateName := fmt.Sprintf("data/exercises/%d/exercise_test.go.tmpl", number)
+		exercise, err := Asset(exerciseTemplateName)
+		if err != nil {
+			panic(err)
+		}
+		exerciseTest, err := Asset(exerciseTestTemplateName)
+		if err != nil {
+			panic(err)
+		}
+
+		exerciseFileName := fmt.Sprintf("data/exercise%d.go", number)
+		exerciseTestFileName := fmt.Sprintf("data/exercise%d_test.go", number)
+
+		ioutil.WriteFile(exerciseFileName, exercise, 0644)
+		ioutil.WriteFile(exerciseTestFileName, exerciseTest, 0644)
+
+		http.Redirect(w, r, "/instructions", http.StatusSeeOther)
+	})
+
+	http.HandleFunc("/instructions", func(w http.ResponseWriter, r *http.Request) {
+		instructionsTemplate := fmt.Sprintf("data/exercises/%d/instructions.md.tmpl", 1)
+		instructions, err := Asset(instructionsTemplate)
+		if err != nil {
+			panic(err)
+		}
+		output := blackfriday.Run(instructions)
+		fmt.Fprint(w, string(output))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
